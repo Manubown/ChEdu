a436eab03b1afc060e97ccca416a18b94b6f5181
Manubown <manuel.gut030603@gmail.com>
2021-09-09 08:11:13 +0200

    Revert "React-Native-Web added"
    This reverts commit c5847b470b80fb0b6c84e14c90d4133f98379bad.

--- App.js
+++ App.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import HomeScreen from '../Pages/Home';
-import IntroScreen from '../Pages/Intro';
-import MainView from '../Pages/MainView';
+import HomeScreen from './Pages/Home';
+import IntroScreen from './Pages/Intro';
+import MainView from './Pages/MainView';
 
 const Stack = createNativeStackNavigator();
 
