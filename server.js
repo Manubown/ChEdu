7d15c27c01a39562152625cc16a7f5501f352a8f
Manubown <55450475+Manubown@users.noreply.github.com>
2021-09-30 11:53:38 +0200

    Update Server.js

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+
 const app = express();
+
+// Certificate
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/chedu.at/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/chedu.at/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync("/etc/letsencrypt/live/chedu.at/chain.pem", "utf8");
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 
 app.use(express.static(path.join(__dirname, "build")));
 
@@ -8,4 +29,13 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(9000);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+  console.log("HTTP Server running on port 80");
+});
+
+httpsServer.listen(443, () => {
+  console.log("HTTPS Server running on port 443");
+});
