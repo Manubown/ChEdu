90775ede7570336355537f6d3342499b60824d35
Manubown <55450475+Manubown@users.noreply.github.com>
2021-10-06 09:46:45 +0200

    TempButton on MainPage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import HomeScreen from './Pages/Home';
-import IntroScreen from './Pages/Intro';
-import MainView from './Pages/MainView';
-import Settings from './Pages/Setting';
-import Login from './Pages/Login';
+import HomeScreen from "./Pages/Home";
+import IntroScreen from "./Pages/Intro";
+import MainView from "./Pages/MainView";
+import Settings from "./Pages/Setting";
+import Login from "./Pages/Login";
+import Temp from "./Pages/TempPage";
 
 const Stack = createNativeStackNavigator();
 
@@ -17,16 +18,16 @@ const MyStack = () => {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: 'ChEdu' }}
+          options={{ title: "ChEdu" }}
         />
         <Stack.Screen name="Intro" component={IntroScreen} />
         <Stack.Screen name="MainView" component={MainView} />
         <Stack.Screen name="Setting" component={Settings} />
         <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Temp" component={Temp} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
 
 export default MyStack;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
-import { Dimensions } from 'react-native';
+import { Dimensions } from "react-native";
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 //SetUp Links:
 // https://aureliomerenda.medium.com/create-a-native-web-app-with-react-native-web-419acac86b82
 // https://stackoverflow.com/questions/55235825/error-failed-to-build-ios-project-we-ran-xcodebuild-command-but-it-exited-wit
 
-const Home = ({ navigation }) =>{
-    return (
-        <View style={styles.Container}>
-                <View style={{flex:1,justifyContent: "center", alignItems: "center" }}>
-                    <View style={ styles.BaseShadow}>
-                      <Text style = {styles.Title}>
-                          <Text style = {styles.CheduBlue}>Ch</Text>
-                          <Text style = {styles.CheduDarkBlue}>Edu</Text>
-                      </Text>
-                      <Text>
-                          Learn how to play chess!
-                      </Text>
-                    </View>
-                    <View style={styles.BaseShadow}>
-                        <Text>Welcome to our Diploma Project, we are currently in development!</Text>
-                    </View>
-                    <View style={styles.BaseShadow}>
-                        <Text>Thank you for visiting our Site!</Text>
-                    </View>
-                </View>
+const Home = ({ navigation }) => {
+  return (
+    <View style={styles.Container}>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View style={styles.BaseShadow}>
+          <Text style={styles.Title}>
+            <Text style={styles.CheduBlue}>Ch</Text>
+            <Text style={styles.CheduDarkBlue}>Edu</Text>
+          </Text>
+          <Text>Learn how to play chess!</Text>
         </View>
-    );
-}
+        <View style={styles.BaseShadow}>
+          <Text>
+            Welcome to our Diploma Project, we are currently in development!
+          </Text>
+        </View>
+        <View style={styles.BaseShadow}>
+          <Text>Thank you for visiting our Site!</Text>
+        </View>
+        <Button
+          onPress={() => {
+            navigation.navigate("Temp"); //MainView
+          }}
+          title="Start now!"
+        />
+      </View>
+    </View>
+  );
+};
 /*
 <View style={{flexDirection:'row', justifyContent:'center', alignItems:"center"}}>
                   <View style = {styles.BottonVield}>
@@ -62,65 +68,62 @@ const Home = ({ navigation }) =>{
                 </View>
 */
 
-calculateView => {
-    
-}
+(calculateView) => {};
 
 const styles = StyleSheet.create({
-    Container: {
-      flexGrow:1,
-      //flexDirection: 'row',
+  Container: {
+    flexGrow: 1,
+    //flexDirection: 'row',
+  },
+  Horizontal: {
+    flexDirection: "row",
+  },
+
+  Title: {
+    fontWeight: "bold",
+    fontSize: 100,
+  },
+  CheduBlue: {
+    color: "#00578a",
+  },
+  CheduDarkBlue: {
+    color: "#0e113f",
+  },
+
+  BottonVield: {
+    height: 50,
+    width: 200,
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignSelf: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    Horizontal: {
-        flexDirection: 'row'
+    shadowOpacity: 0.23,
+    shadowRadius: 5,
+    elevation: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  BaseShadow: {
+    margin: 10,
+    padding: 20,
+    borderRadius: 20,
+    backgroundColor: "white",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
 
-      Title: {
-        fontWeight: 'bold',
-        fontSize: 100,
-        
-      },
-      CheduBlue:{
-        color: '#00578a'
-      },
-      CheduDarkBlue:{
-        color: '#0e113f'
-      },
-    
-      BottonVield:{
-        height:50,
-        width:200,
-        margin:20,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        alignSelf:'center',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 5,
-        elevation: 4,
-        justifyContent: "center", 
-        alignItems: "center" 
-      },
-    
-      BaseShadow:{
-      margin:10,
-        padding:20,
-        borderRadius: 20,
-        backgroundColor: 'white',
-        alignItems:"center",
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-    
-        shadowOpacity: 0.23,
-        shadowRadius: 5,
-        elevation: 4,
-      }
+    shadowOpacity: 0.23,
+    shadowRadius: 5,
+    elevation: 4,
+  },
 });
 export default Home;
