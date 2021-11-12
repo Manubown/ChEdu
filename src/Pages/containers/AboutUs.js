b70994f5bf95d79216bc8003a69243d714703070
Manubown <55450475+Manubown@users.noreply.github.com>
2021-11-12 08:24:06 +0100

    Create AboutUs.js

---
+++ src/Pages/containers/AboutUs.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+
+import { Dimensions } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const x = 100;
+const y = 200;
+
+const AboutUs = ({ navigation }) => {
+  return;
+  <View style={{ flexGrow: 1 }}>
+    <Text>About Us Page!</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AboutUs;
